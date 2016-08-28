f7868198cbcac72e9d320661bbc9a8b1ae06bab7
Satrajit Ghosh <satrajit.ghosh@gmail.com>
2016-08-28 13:31:39 -0400

    Update cmrr_heuristic.py

--- heuristics/cmrr_heuristic.py
+++ heuristics/cmrr_heuristic.py
@@ -5,56 +5,58 @@ def create_key(template, outtype=('nii.gz','dicom'), annotation_classes=None):
         raise ValueError('Template must be a valid format string')
     return (template, outtype, annotation_classes)
 
-def infotodict(seqinfo):
-    """Heuristic evaluator for determining which runs belong where
 
-    allowed template fields - follow python string module: 
-
-    item: index within category 
-    subject: participant id 
-    seqitem: run number during scanning
-    subindex: sub index within group
-    """
     t1 = create_key('anat/sub-{subject}_T1w')
     t2 = create_key('anat/sub-{subject}_T2w')
-    rest_ap = create_key('func/sub-{subject}_dir-AP_task-rest_run-{item:02d}_bold')
-    rest_pa = create_key('func/sub-{subject}_dir-PA_task-rest_run-{item:02d}_bold')
-    face_ap = create_key('func/sub-{subject}_task-face_run-{item:02d}_acq-AP_bold')
-    face_pa = create_key('func/sub-{subject}_task-face_run-{item:02d}_acq-PA_bold')
-    gamble_ap = create_key('func/sub-{subject}_task-gambling_run-{item:02d}_acq-AP_bold')
-    gamble_pa = create_key('func/sub-{subject}_task-gambling_run-{item:02d}_acq-PA_bold')
+    rest = create_key('func/sub-{subject}_dir-{acq}_task-rest_run-{item:02d}_bold')
+    face = create_key('func/sub-{subject}_task-face_run-{item:02d}_acq-{acq}_bold')
+    gamble = create_key('func/sub-{subject}_task-gambling_run-{item:02d}_acq-{acq}_bold')
     conflict = create_key('func/sub-{subject}_task-conflict_run-{item:02d}_acq-{acq}_bold')
-    dwi_ap = create_key('dwi/sub-{subject}_dir-AP_dwi')
-    dwi_pa = create_key('dwi/sub-{subject}_dir-PA_dwi')
+    dwi = create_key('dwi/sub-{subject}_dir-{acq}_run-{item:02d}_dwi')
 
-    info = {t1:[],t2:[],rest_ap:[],rest_pa:[],face_ap:[],face_pa:[],gamble_ap:[],gamble_pa:[],
-            conflict:[], dwi_ap:[],dwi_pa:[]}
+    fmap_rest = create_key('fmap/sub-{subject}_acq-func{acq}_dir-{dir}_run-{item:02d}_epi')
+    fmap_dwi = create_key('fmap/sub-{subject}_acq-dwi{acq}_dir-{dir}_run-{item:02d}_epi')
 
-    for s in seqinfo:
+    info = {t1:[], t2:[], rest:[], face:[], gamble:[], conflict:[], dwi:[], fmap_rest:[], fmap_dwi:[]}
+
+    for idx, s in enumerate(seqinfo):
         x,y,sl,nt = (s[6],s[7],s[8],s[9])
         if (sl == 208) and (nt == 1) and ('T1w' in s[12]):
-            info[t1].append([s[2]])
+            info[t1] = [s[2]]
         if (sl == 208) and ('T2w' in s[12]):
-            info[t2].append([s[2]])
-        if (nt == 1) and ('dMRI_dir99_AP' in s[12]):
-            info[dwi_ap].append([s[2]])
-        if (nt == 1) and ('dMRI_dir99_PA' in s[12]):
-            info[dwi_pa].append([s[2]])
+            info[t2] = [s[2]]
+        if (nt >= 99) and (('dMRI_dir98_AP' in s[12]) or ('dMRI_dir99_AP' in s[12])):
+            acq = s[12].split('dMRI_')[1].split('_')[0] + 'AP'
+            info[dwi].append({'item': s[2], 'acq': acq})
+        if (nt >= 99) and (('dMRI_dir98_PA' in s[12]) or ('dMRI_dir99_PA' in s[12])):
+            acq = s[12].split('dMRI_')[1].split('_')[0] + 'PA'
+            info[dwi].append({'item': s[2], 'acq': acq})
+        if (nt == 1) and (('dMRI_dir98_AP' in s[12]) or ('dMRI_dir99_AP' in s[12])):
+            acq = s[12].split('dMRI_')[1].split('_')[0]
+            info[fmap_dwi].append({'item': s[2], 'dir': 'AP', 'acq': acq})
+        if (nt == 1) and (('dMRI_dir98_PA' in s[12]) or ('dMRI_dir99_PA' in s[12])):
+            acq = s[12].split('dMRI_')[1].split('_')[0]
+            info[fmap_dwi].append({'item': s[2], 'dir': 'PA', 'acq': acq})
         if (nt == 420) and ('rfMRI_REST_AP' in s[12]):
-            info[rest_ap].append([s[2]])
+            info[rest].append({'item': s[2], 'acq': 'AP'})
         if (nt == 420) and ('rfMRI_REST_PA' in s[12]):
-            info[rest_pa].append([s[2]])
+            info[rest].append({'item': s[2], 'acq': 'PA'})
+        if (nt == 1) and ('rfMRI_REST_AP' in s[12]):
+            if seqinfo[idx + 1][9] != 420:
+                continue
+            info[fmap_rest].append({'item': s[2], 'dir': 'AP', 'acq': ''})
+        if (nt == 1) and ('rfMRI_REST_PA' in s[12]):
+            info[fmap_rest].append({'item': s[2], 'dir': 'PA', 'acq': ''})
         if (nt == 346) and ('tfMRI_faceMatching_AP' in s[12]):
-            info[face_ap].append([s[2]])
+            info[face].append({'item': s[2], 'acq': 'AP'})
         if (nt == 346) and ('tfMRI_faceMatching_PA' in s[12]):
-            info[face_pa].append([s[2]])
+            info[face].append({'item': s[2], 'acq': 'PA'})
         if (nt == 288) and ('tfMRI_conflict_AP' in s[12]):
-            # Add your own variables to the template using a dictionary
             info[conflict].append({'item': s[2], 'acq': 'AP'})
         if (nt == 288) and ('tfMRI_conflict_PA' in s[12]):
             info[conflict].append({'item': s[2], 'acq': 'PA'})
         if (nt == 223) and ('tfMRI_gambling_AP' in s[12]):
-            info[gamble_ap].append([s[2]])
+            info[gamble].append({'item': s[2], 'acq': 'AP'})
         if (nt == 223) and ('tfMRI_gambling_PA' in s[12]):
-            info[gamble_pa].append([s[2]])
+            info[gamble].append({'item': s[2], 'acq': 'PA'})
     return info
