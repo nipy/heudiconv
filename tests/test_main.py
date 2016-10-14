da8e8b5b1b57b8fb53c18cb6b2cc5c87502c3339
Yaroslav Halchenko <debian@onerussian.com>
2016-10-13 21:16:57 -0400

    ENH+BF: few basic tests and a little bugfix already ;-)

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,11 @@
+import pytest
 import sys
 
 from mock import patch
+from os.path import exists
 from six.moves import StringIO
-import pytest
+
+
 from . import heudiconv
 
 
@@ -18,3 +21,41 @@ def test_main_version(std):
     with pytest.raises(SystemExit):
         heudiconv.main(['--version'])
     assert std.getvalue().rstrip() == heudiconv.__version__
+
+
+def test_create_file_if_missing(tmpdir):
+    tf = tmpdir.join("README.txt")
+    assert not tf.exists()
+    heudiconv.create_file_if_missing(str(tf), "content")
+    assert tf.exists()
+    assert tf.read() == "content"
+    heudiconv.create_file_if_missing(str(tf), "content2")
+    # nothing gets changed
+    assert tf.read() == "content"
+
+
+def test_populate_bids_templates(tmpdir):
+    heudiconv.populate_bids_templates(str(tmpdir))
+    for f in "README", "dataset_description.json", "CHANGES":
+        # Just test that we have created them and they all have stuff TODO
+        assert "TODO" in tmpdir.join(f).read()
+
+
+def test_add_participant_record(tmpdir):
+    tf = tmpdir.join('participants.tsv')
+    assert not tf.exists()
+    heudiconv.add_participant_record(str(tmpdir), "sub01", "023Y", "M")
+    # should create the file and place corrected record
+    sub01 = tf.read()
+    assert sub01 == """\
+participant_id	age	sex	group
+sub-sub01	23	M	control
+"""
+    heudiconv.add_participant_record(str(tmpdir), "sub01", "023Y", "F")
+    assert tf.read() == sub01  # nothing was added even though differs in values
+    heudiconv.add_participant_record(str(tmpdir), "sub02", "2", "F")
+    assert tf.read() == """\
+participant_id	age	sex	group
+sub-sub01	23	M	control
+sub-sub02	2	F	control
+"""
