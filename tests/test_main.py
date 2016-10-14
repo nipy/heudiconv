f886e9281fe4f2fefd7df7d0d7b169809dc703c1
Yaroslav Halchenko <debian@onerussian.com>
2016-10-13 20:57:22 -0400

    ENH: let's try to use pytest

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,18 +2,19 @@ import sys
 
 from mock import patch
 from six.moves import StringIO
-from nose.tools import assert_raises, assert_equal
-
+import pytest
 from . import heudiconv
 
 
 @patch('sys.stdout', new_callable=StringIO)
 def test_main_help(stdout):
-    assert_raises(SystemExit, heudiconv.main, ['--help'])
-    assert(stdout.getvalue().startswith("usage: "))
+    with pytest.raises(SystemExit):
+        heudiconv.main(['--help'])
+    assert stdout.getvalue().startswith("usage: ")
 
 
 @patch('sys.stderr' if sys.version_info[:2] <= (3, 3) else 'sys.stdout', new_callable=StringIO)
 def test_main_version(std):
-    assert_raises(SystemExit, heudiconv.main, ['--version'])
-    assert_equal(std.getvalue().rstrip(), heudiconv.__version__)
+    with pytest.raises(SystemExit):
+        heudiconv.main(['--version'])
+    assert std.getvalue().rstrip() == heudiconv.__version__
