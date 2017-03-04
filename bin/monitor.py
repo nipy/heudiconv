74dc957a78c5b33e6ca25fe2f16cd6de76092ddc
Matteo Visconti dOC <mvdoc.gr@dartmouth.edu>
2017-03-04 15:19:29 -0500

    Add database logging

--- bin/monitor.py
+++ bin/monitor.py
@@ -1,41 +1,50 @@
 #!/usr/bin/env python
 import logging
+import os
+import re
+import subprocess
+import time
+
+from collections import deque
+from datetime import date
 import inotify.adapters
 from inotify.constants import IN_MODIFY, IN_CREATE, IN_ISDIR
-from collections import deque
-import os
-from datetime import date
-import re
-import time
+from tinydb import TinyDB
 
 _DEFAULT_LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 _LOGGER = logging.getLogger(__name__)
 
 MASK = (IN_MODIFY | IN_CREATE)
 MASK_NEWDIR = (IN_CREATE | IN_ISDIR)
-WAIT_TIME = 10  # in seconds
+WAIT_TIME = 2  # in seconds
 
+# open database
+db = TinyDB('database.json')
 
 def _configure_logging():
     _LOGGER.setLevel(logging.DEBUG)
-
     ch = logging.StreamHandler()
-
     formatter = logging.Formatter(_DEFAULT_LOG_FORMAT)
     ch.setFormatter(formatter)
-
     _LOGGER.addHandler(ch)
 
 
 def process(paths2process):
+    cmd = 'echo heudiconv {0}'
     if paths2process and time.time() - os.path.getmtime(paths2process[0]) > WAIT_TIME:
-        process_me = paths2process.popleft()
-        print("Time to process {0}".format(process_me))
-        time.sleep(3)
-        print("Done processing {0}".format(process_me))
+        process_me = paths2process.popleft().decode('utf-8')
+        cmd_ = cmd.format(process_me)
+        try:
+            print("Time to process {0}".format(process_me))
+            subprocess.check_call(cmd_.split())
+            _LOGGER.info("Done running {0}".format(cmd_))
+            # here we should inspect output and then store additional info
+            db.insert({'input_path': process_me, 'success': 1, 'subject_id': 'TODO', 'output_path': 'TODO'})
+        except subprocess.CalledProcessError:
+            _LOGGER.error("{0} failed".format(cmd_))
+            db.insert({'input_path': process_me, 'success': 0})
 
 
-#"inbox/DICOM" "/20../../.."
 def monitor(topdir='/tmp/new_dir', check_ptrn='/20../../..'):
     paths2process = deque()
     # watch only today's folder
