463d9027d23f67788c1a9120fa9ced5e3ad7937a
Matteo Visconti dOC <matteo.visconti.gr@dartmouth.edu>
2017-06-24 15:13:25 -0700

    Reload scans keys if existing, start adding tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -115,11 +115,24 @@ def test_get_formatted_scans_key_row():
     ]
     outname_bids_file = '/a/path/Halchenko/Yarik/950_bids_test4/sub-phantom1sid1/fmap/sub-phantom1sid1_acq-3mm_phasediff.json'
 
-    row = heudiconv.get_formatted_scans_key_row(item, outname_bids_file)
+    row = heudiconv.get_formatted_scans_key_row(item)
     assert(len(row) == 3)
     assert(row[0] == '2016-10-14T09:26:34')
     assert(row[1] == '')
     randstr1 = row[2]
-    row = heudiconv.get_formatted_scans_key_row(item, outname_bids_file)
+    row = heudiconv.get_formatted_scans_key_row(item)
     randstr2 = row[2]
     assert(randstr1 != randstr2)
+
+
+# TODO: finish this
+def test_add_rows_to_scans_keys_file(tmpdir):
+    fn = opj(tmpdir.strpath, 'file.tsv')
+    rows = {
+        'my_file.nii.gz': ['2016adsfasd', '', 'fasadfasdf'],
+        'another_file.nii.gz': ['2018xxxxx', '', 'fasadfasdf']
+    }
+    heudiconv.add_rows_to_scans_keys_file(fn, rows)
+
+    #with open(fn, 'r') as csvfile:
+
