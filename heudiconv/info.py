2b3cc5755a3dd0fe230b13f8df293ae02c79de57
mathiasg <mathiasg@mit.edu>
2017-10-11 17:35:17 -0400

    rf: update package information/setup

--- heudiconv/info.py
+++ heudiconv/info.py
@@ -1,7 +1,28 @@
-__version__ = '0.4'
-__author__ = 'Heudiconv team and contributors'
-__url__ = 'https://github.com/nipy/heudiconv'
+__version__ = "0.4"
+__author__ = "Heudiconv team and contributors"
+__url__ = "https://github.com/nipy/heudiconv"
 __packagename__ = 'heudiconv'
-__description__ = '''Convert DICOM dirs based on heuristic info - Heudiconv 
+__description__ = "Heuristic DICOM Converter"
+__longdesc__ = """Convert DICOM dirs based on heuristic info - Heudiconv
 uses the dcmstack package and dcm2niix tool to convert DICOM directories or
-tarballs into collections of NIfTI files following pre-defined heuristic(s).'''
+tarballs into collections of NIfTI files following pre-defined heuristic(s)."""
+
+REQUIRES = [
+    'nibabel',
+    'pydicom',
+    'nipype',
+    'dcmstack',
+]
+
+TESTS_REQUIRES = [
+    'six',
+    'pytest',
+]
+
+EXTRA_REQUIRES = {
+    'tests': TESTS_REQUIRES,
+    'monitor': ['inotify', 'tinydb'],
+    'datalad': ['datalad']
+}
+
+EXTRA_REQUIRES['all'] = [val for _, val in list(EXTRA_REQUIRES.items())]
