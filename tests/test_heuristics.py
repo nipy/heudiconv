102d47d6385f2a3f41706daaf2d700847778a007
mathiasg <mathiasg@mit.edu>
2017-10-16 17:42:26 -0400

    tst: updating testing with new locations

--- tests/test_heuristics.py
+++ tests/test_heuristics.py
@@ -1,4 +1,4 @@
-from . import heudiconv
+from heudiconv.cli.run import main as runner
 
 import os
 from mock import patch
@@ -11,11 +11,14 @@ import re
 
 import pytest
 
-from datalad.api import Dataset
+try:
+    from datalad.api import Dataset
+except ImportError:
+    Dataset = None
 
-
+# this will fail if not in project's root directory
 def test_smoke_converall(tmpdir):
-    heudiconv.main(
+    runner(
         ("-f heuristics/convertall.py -c dcm2niix -o %s -b --datalad "
          "-s fmap_acq-3mm -d tests/data/{subject}/*" % tmpdir).split(' ')
     )
@@ -24,10 +27,11 @@ def test_smoke_converall(tmpdir):
 @pytest.mark.parametrize('heuristic', [ 'dbic_bids', 'convertall' ])
 @pytest.mark.parametrize(
     'invocation', [
-        "tests/data",    # our new way with automated groupping
+        "--files tests/data",    # our new way with automated groupping
         "-d tests/data/{subject}/* -s 01-fmap_acq-3mm" # "old" way specifying subject
         # should produce the same results
     ])
+@pytest.mark.skipif(Dataset is None, reason="no datalad")
 def test_dbic_bids_largely_smoke(tmpdir, heuristic, invocation):
     is_bids = True if heuristic == 'dbic_bids' else False
     arg = "-f heuristics/%s.py -c dcm2niix -o %s" % (heuristic, tmpdir)
@@ -52,16 +56,16 @@ def test_dbic_bids_largely_smoke(tmpdir, heuristic, invocation):
     if heuristic != 'dbic_bids' and invocation != 'tests/data':
         # those guys -- they just plow through it ATM without failing, i.e.
         # the logic is to reprocess
-        heudiconv.main(args)
+        runner(args)
     else:
         with pytest.raises(RuntimeError):
-            heudiconv.main(args)
+            runner(args)
     # but there should be nothing new
     assert not ds.repo.dirty
     assert head == ds.repo.get_hexsha()
 
     # unless we pass 'overwrite' flag
-    heudiconv.main(args + ['--overwrite'])
+    runner(args + ['--overwrite'])
     # but result should be exactly the same, so it still should be clean
     # and at the same commit
     assert ds.is_installed()
@@ -71,12 +75,12 @@ def test_dbic_bids_largely_smoke(tmpdir, heuristic, invocation):
 
 @pytest.mark.parametrize(
     'invocation', [
-        "tests/data",    # our new way with automated groupping
+        "--files tests/data",    # our new way with automated groupping
     ])
 def test_scans_keys_dbic_bids(tmpdir, invocation):
     args = "-f heuristics/dbic_bids.py -c dcm2niix -o %s -b " % tmpdir
     args += invocation
-    heudiconv.main(args.split())
+    runner(args.split())
     # for now check it exists
     scans_keys = glob(pjoin(tmpdir.strpath, '*/*/*/*/*.tsv'))
     assert(len(scans_keys) == 1)
@@ -95,8 +99,8 @@ def test_scans_keys_dbic_bids(tmpdir, invocation):
 
 @patch('sys.stdout', new_callable=StringIO)
 def test_ls(stdout):
-    args = "-f heuristics/dbic_bids.py --command ls tests/data".split(' ')
-    heudiconv.main(args)
+    args = "-f heuristics/dbic_bids.py --command ls --files tests/data".split(' ')
+    runner(args)
     out = stdout.getvalue()
     assert 'StudySessionInfo(locator=' in out
     assert 'Halchenko/Yarik/950_bids_test4' in out
--- tests/test_tarballs.py
+++ tests/test_tarballs.py
@@ -9,26 +9,33 @@ from os.path import dirname
 from six.moves import StringIO
 from glob import glob
 
-from . import heudiconv
+from heudiconv.dicoms import compress_dicoms
+from heudiconv.utils import TempDirs
+
 from .utils import md5sum
 
 tests_datadir = opj(dirname(__file__), 'data')
 
 
 def test_reproducibility(tmpdir):
-    #heudiconv.compress_dicoms(dicom_list, prefix, sourcedir)
     prefix = str(tmpdir.join("precious"))
-    args = [glob(opj(tests_datadir, '01-fmap_acq-3mm', '*')), prefix]
-    tarball = heudiconv.compress_dicoms(*args)
+    args = [glob(opj(tests_datadir, '01-fmap_acq-3mm', '*')),
+            prefix,
+            TempDirs(),
+            True]
+    tarball = compress_dicoms(*args)
     md5 = md5sum(tarball)
     assert tarball
-    # must not override by default
-    with pytest.raises(RuntimeError):
-        heudiconv.compress_dicoms(*args)
+    # must not override, ensure overwrite is set to False
+    args[-1] = False
+    assert compress_dicoms(*args) is None
+    # reset this
+    args[-1] = True
+
     os.unlink(tarball)
 
     time.sleep(1.1)  # need to guarantee change of time
-    tarball_ = heudiconv.compress_dicoms(*args)
+    tarball_ = compress_dicoms(*args)
     md5_ = md5sum(tarball_)
     assert tarball == tarball_
     assert md5 == md5_
