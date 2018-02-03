2279b6026a59cfa5f327214bb513b55ad71b050a
Yaroslav Halchenko <debian@onerussian.com>
2018-02-02 23:16:18 -0500

    BF: take first path from heuristics module

--- tests/utils.py
+++ tests/utils.py
@@ -1,7 +1,7 @@
 import os.path as op
 import heudiconv.heuristics
 
-HEURISTICS_PATH = op.join(heudiconv.heuristics.__path__)
+HEURISTICS_PATH = op.join(heudiconv.heuristics.__path__[0])
 TESTS_DATA_PATH = op.join(op.dirname(__file__), 'data')
 
 
