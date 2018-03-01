d18cacb20d41d401c557642f038bb7c2d33e2bfe
Yaroslav Halchenko <debian@onerussian.com>
2018-02-28 22:50:51 -0500

    reraise is import of dcmstack is failing for other natural causes

--- heudiconv/external/dcmstack.py
+++ heudiconv/external/dcmstack.py
@@ -7,13 +7,14 @@ from .pydicom import dcm  # to assure that we have it one way or another
 try:
     import dcmstack as ds
 except ImportError as e:
-    if "No module named dicom" in str(e):
-        # a butt plug due to rename of dicom -> pydicom
-        import sys
-        if "dicom" in sys.modules:
-            from heudiconv import lgr
-            lgr.warning("unexpected happened -- dicom module already loaded")
-            raise  # reraise since something is not right really
-        sys.modules['dicom'] = dcm
-        # and try again
-        import dcmstack as ds
+    if "No module named dicom" not in str(e):
+        raise
+    # a butt plug due to rename of dicom -> pydicom
+    import sys
+    if "dicom" in sys.modules:
+        from heudiconv import lgr
+        lgr.warning("unexpected happened -- dicom module already loaded")
+        raise  # reraise since something is not right really
+    sys.modules['dicom'] = dcm
+    # and try again
+    import dcmstack as ds
