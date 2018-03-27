7f3508577c81ba88781a3cd7ddf05b8099b858c6
Yaroslav Halchenko <debian@onerussian.com>
2018-03-27 14:40:59 -0400

    ENH: make utils/test-compare-two-versions.sh usable as a simple prefix to heudiconv call
    now that even the released version has --files and we could compare
    for any heuristic

--- utils/test-compare-two-versions.sh
+++ utils/test-compare-two-versions.sh
@@ -7,7 +7,7 @@
 PS1=+
 set -eu
 
-outdir=${OUTDIR:=compare-versions/$(basename $1)}
+outdir=${OUTDIR:=compare-versions}
 
 RUN=echo
 RUN=
@@ -33,14 +33,15 @@ function run() {
    $RUN heudiconv --random-seed 1 -c dcm2niix -o $out --datalad --bids "$@" >| $out.log 2>&1
 }
 
-run heudiconv        rolando -f heudiconv/heuristics/dbic_bids.py "$@"
-run heudiconv-master master  -f reproin                   --files "$@"
+other=other
+run heudiconv        $other "$@"
+run heudiconv-master master "$@"
 
 cd $outdir
 #git remote add rolando "$outdir/rolando"
 #git fetch rolando
 # git diff --stat rolando/master..
-if diff  -Naur --exclude=.git --ignore-matching-lines='^\s*id\s*=.*' rolando master >| diff.patch; then
+if diff  -Naur --exclude=.git --ignore-matching-lines='^\s*id\s*=.*' $other master >| diff.patch; then
     echo "Results are identical"
 else
     echo "Results differ: $PWD/diff.patch"
