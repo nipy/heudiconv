09e8850968f1ab6bbd894cb40b87772c168f52c6
leej3 <johnleenimh@gmail.com>
2018-07-09 10:13:19 -0400

    add template for tests to confirm grouping behaviour

---
+++ tests/test_grouping.py
@@ -0,0 +1,28 @@
+from datalad import auto,api
+
+# api.install( 'dbic/QA','http://datasets.datalad.org')
+
+# def test_grouping_using_files_flag():
+
+    # with auto.AutomagicIO():
+
+        # define subject explicitly
+
+        # define subject and session explicitly
+
+        # group by all
+
+        # group by tar
+
+        # group by studyUID
+
+        # group by accession number
+
+        # override grouping
+
+
+
+
+
+
+
