ec188021ad39b12c9bf105d41e300e3cc53149a8
Jakub Kaczmarzyk <jakubk@mit.edu>
2018-11-01 18:03:12 -0400

    install heudiconv as editable

--- utils/gen-docker-image.sh
+++ utils/gen-docker-image.sh
@@ -12,5 +12,6 @@ docker run --rm $image generate docker -b neurodebian:stretch -p apt \
     --copy . /src/heudiconv \
     --miniconda use_env=base conda_install="python=3.6 traits>=4.6.0 scipy numpy nomkl" \
       pip_install="/src/heudiconv[all]" \
+      pip_opts="--editable" \
     --entrypoint "heudiconv" \
 > ../Dockerfile
