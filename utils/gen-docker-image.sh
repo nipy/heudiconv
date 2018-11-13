54e0808d2cbf96893fe8b34a9ae536c6da47ab03
mathiasg <mathiasg@mit.edu>
2018-11-13 10:15:50 -0500

    fix: add "recommended" package

--- utils/gen-docker-image.sh
+++ utils/gen-docker-image.sh
@@ -8,7 +8,7 @@ image="kaczmarj/neurodocker:master@sha256:9f7d58f6977cfcd4dd5d1a2e70be4124417206
 
 docker run --rm $image generate docker -b neurodebian:stretch -p apt \
     --dcm2niix version=v1.0.20180622 method=source \
-    --install git gcc pigz liblzma-dev libc-dev git-annex-standalone \
+    --install git gcc pigz liblzma-dev libc-dev git-annex-standalone netbase \
     --copy . /src/heudiconv \
     --miniconda use_env=base conda_install="python=3.6 traits>=4.6.0 scipy numpy nomkl" \
       pip_install="/src/heudiconv[all]" \
