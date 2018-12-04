518ce37e1112ea7234637889b0a924d5bef41728
Dae Houlihan <daeda@mit.edu>
2018-12-04 18:16:37 -0500

    rescuing files for git annex

--- heudiconv/tests/anonymize_script.py
+++ heudiconv/tests/anonymize_script.py
@@ -1,1 +1,19 @@
-/annex/objects/SHA256E-s305--d13f986ddc4e3ba2a9fdea076834eb9861cddd365380d5675ebdf1177281f352.py
+#! /usr/bin/env python 
+
+import sys
+import re
+import ctypes
+
+
+def bids_id_(sid):
+    parsed_id = re.compile(r"^(?:sub-|)(.+)$").search(sid).group(1)
+    return str(ctypes.c_size_t(hash(parsed_id)).value)
+
+
+def main():
+    sid = sys.argv[1]
+    return bids_id_(sid)
+
+
+if __name__ == '__main__':
+    main()
