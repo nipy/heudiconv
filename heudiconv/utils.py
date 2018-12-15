0a14a1da9c8ec3092aabf92a61dd058017d52eb1
Dae Houlihan <daeda@mit.edu>
2018-12-14 19:31:44 -0500

    adding the string check back

--- heudiconv/utils.py
+++ heudiconv/utils.py
@@ -107,8 +107,15 @@ def anonymize_sid(sid, anon_sid_cmd):
     from subprocess import check_output
     
     cmd = [anon_sid_cmd, sid]
-    kwargs = {'encoding': 'utf-8'} if sys.version_info[0] >= 3 else {}
-    return check_output(cmd, **kwargs).strip()
+    shell_return = check_output(cmd)
+
+    ### Handle subprocess returning a bytes literal string to a python3 interpreter
+    if all([sys.version_info[0] > 2, isinstance(shell_return, bytes), isinstance(sid, str)]):
+        anon_sid = shell_return.decode()
+    else:
+        anon_sid = shell_return
+    
+    return anon_sid.strip()
 
 
 def create_file_if_missing(filename, content):
