4c36fb4c8a0ca099234934c755fe3c242610a677
Yaroslav Halchenko <debian@onerussian.com>
2019-05-14 12:44:11 -0400

    ENH(TST): basic testing for WIP stripping and (date)->{date} tune up

--- heudiconv/heuristics/test_reproin.py
+++ heudiconv/heuristics/test_reproin.py
@@ -166,6 +166,7 @@ def test_parse_series_spec():
     assert \
         pdpn(" PREFIX:bids_func_ses+_task-boo_run+  ") == \
         pdpn("PREFIX:bids_func_ses+_task-boo_run+") == \
+        pdpn("WIP func_ses+_task-boo_run+") == \
         pdpn("bids_func_ses+_run+_task-boo") == \
            {
                'seqtype': 'func',
@@ -202,3 +203,9 @@ def test_parse_series_spec():
                 'acq': 'MPRAGE',
                 'seqtype_label': 'T1w'
            }
+
+    # Check for currently used {date}, which should also should get adjusted
+    # from (date) since Philips does not allow for {}
+    assert pdpn("func_ses-{date}") == \
+           pdpn("func_ses-(date)") == \
+           {'seqtype': 'func', 'session': '{date}'}
