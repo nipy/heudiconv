46c0bdb5531399ea54ccc9334fedd4dedb3b41b2
pvelasco <pablo.velasco@nyu.edu>
2021-01-08 14:46:21 -0500

    ENH: Allows the user to save the Phoenix Report (Siemens) in the sourcedata
    This way the user can go back to the scanner and import the exact same protocol that was run for a given subject, improving reproducibility.

--- heudiconv/dicoms.py
+++ heudiconv/dicoms.py
@@ -265,8 +265,13 @@ def group_dicoms_into_seqinfos(files, grouping, file_filter=None,
         series_id = '-'.join(map(str, series_id))
         if mw.image_shape is None:
             # this whole thing has no image data (maybe just PSg DICOMs)
-            # nothing to see here, just move on
-            continue
+            # If this is a Siemens PhoenixZipReport or PhysioLog, keep it:
+            if mw.dcm_data.SeriesDescription == 'PhoenixZIPReport':
+                # give it a dummy shape, so that we can continue:
+                mw.image_shape = (0, 0, 0)
+            else:
+                # nothing to see here, just move on
+                continue
         seqinfo = create_seqinfo(mw, series_files, series_id)
 
         if per_studyUID:
