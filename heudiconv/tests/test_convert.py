ae61382c021031c000d87e9c8ea27ebd435e165d
pvelasco <pablo.velasco@nyu.edu>
2021-05-03 10:38:32 -0400

    Revert to importing specific functions in test_convert.
    These changes were made in 47bab3c8e07c818dec493b8dcfede767534c710a.

--- heudiconv/tests/test_convert.py
+++ heudiconv/tests/test_convert.py
@@ -6,6 +6,11 @@ from glob import glob
 import pytest
 from .utils import TESTS_DATA_PATH
 
+from heudiconv.convert import (update_complex_name,
+                               update_multiecho_name,
+                               update_uncombined_name,
+                               DW_IMAGE_IN_FMAP_FOLDER_WARNING,
+                               )
 import heudiconv.convert
 from heudiconv.bids import BIDSError
 from heudiconv.utils import load_heuristic
@@ -21,17 +26,17 @@ def test_update_complex_name():
     metadata = {'ImageType': ['ORIGINAL', 'PRIMARY', 'P', 'MB', 'TE3', 'ND', 'MOSAIC']}
     suffix = 3
     out_fn_true = 'sub-X_ses-Y_task-Z_run-01_part-phase_sbref'
-    out_fn_test = heudiconv.convert.update_complex_name(metadata, fn, suffix)
+    out_fn_test = update_complex_name(metadata, fn, suffix)
     assert out_fn_test == out_fn_true
     # Catch an unsupported type and *do not* update
     fn = 'sub-X_ses-Y_task-Z_run-01_phase'
-    out_fn_test = heudiconv.convert.update_complex_name(metadata, fn, suffix)
+    out_fn_test = update_complex_name(metadata, fn, suffix)
     assert out_fn_test == fn
     # Data type is missing from metadata so use suffix
     fn = 'sub-X_ses-Y_task-Z_run-01_sbref'
     metadata = {'ImageType': ['ORIGINAL', 'PRIMARY', 'MB', 'TE3', 'ND', 'MOSAIC']}
     out_fn_true = 'sub-X_ses-Y_task-Z_run-01_part-3_sbref'
-    out_fn_test = heudiconv.convert.update_complex_name(metadata, fn, suffix)
+    out_fn_test = update_complex_name(metadata, fn, suffix)
     assert out_fn_test == out_fn_true
     # Catch existing field with value that *does not match* metadata
     # and raise Exception
@@ -39,7 +44,7 @@ def test_update_complex_name():
     metadata = {'ImageType': ['ORIGINAL', 'PRIMARY', 'P', 'MB', 'TE3', 'ND', 'MOSAIC']}
     suffix = 3
     with pytest.raises(BIDSError):
-        assert heudiconv.convert.update_complex_name(metadata, fn, suffix)
+        assert update_complex_name(metadata, fn, suffix)
 
 
 def test_update_multiecho_name():
@@ -52,15 +57,15 @@ def test_update_multiecho_name():
                 'EchoNumber': 1}
     echo_times = [0.01, 0.02, 0.03]
     out_fn_true = 'sub-X_ses-Y_task-Z_run-01_echo-1_bold'
-    out_fn_test = heudiconv.convert.update_multiecho_name(metadata, fn, echo_times)
+    out_fn_test = update_multiecho_name(metadata, fn, echo_times)
     assert out_fn_test == out_fn_true
     # EchoNumber field is missing from metadata, so use echo_times
     metadata = {'EchoTime': 0.01}
-    out_fn_test = heudiconv.convert.update_multiecho_name(metadata, fn, echo_times)
+    out_fn_test = update_multiecho_name(metadata, fn, echo_times)
     assert out_fn_test == out_fn_true
     # Catch an unsupported type and *do not* update
     fn = 'sub-X_ses-Y_task-Z_run-01_phasediff'
-    out_fn_test = heudiconv.convert.update_multiecho_name(metadata, fn, echo_times)
+    out_fn_test = update_multiecho_name(metadata, fn, echo_times)
     assert out_fn_test == fn
 
 
@@ -73,12 +78,12 @@ def test_update_uncombined_name():
     metadata = {'CoilString': 'H1'}
     channel_names = ['H1', 'H2', 'H3', 'HEA;HEP']
     out_fn_true = 'sub-X_ses-Y_task-Z_run-01_ch-01_bold'
-    out_fn_test = heudiconv.convert.update_uncombined_name(metadata, fn, channel_names)
+    out_fn_test = update_uncombined_name(metadata, fn, channel_names)
     assert out_fn_test == out_fn_true
     # CoilString field has no number in it
     metadata = {'CoilString': 'HEA;HEP'}
     out_fn_true = 'sub-X_ses-Y_task-Z_run-01_ch-04_bold'
-    out_fn_test = heudiconv.convert.update_uncombined_name(metadata, fn, channel_names)
+    out_fn_test = update_uncombined_name(metadata, fn, channel_names)
     assert out_fn_test == out_fn_true
 
 
@@ -99,9 +104,7 @@ def test_b0dwi_for_fmap(tmpdir, capfd):
     # assert that it raised a warning that the fmap directory will contain
     # bvec and bval files.
     output = capfd.readouterr().err.split('\n')
-    expected_msg = heudiconv.convert.DW_IMAGE_IN_FMAP_FOLDER_WARNING.format(
-        folder=op.join(tmppath, 'sub-%s', 'fmap') % subID
-    )
+    expected_msg = DW_IMAGE_IN_FMAP_FOLDER_WARNING.format(folder=op.join(tmppath, 'sub-%s', 'fmap') % subID)
     assert [o for o in output if expected_msg in o]
 
     # check that both 'fmap' and 'dwi' directories have been extracted and they contain
