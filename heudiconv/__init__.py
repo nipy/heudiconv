3258c9f56e1343e5b72da2cb0cb3432ea79e5a08
mathiasg <mathiasg@stanford.edu>
2021-05-07 00:17:31 -0400

    ENH: Top level tempdir tracker

--- heudiconv/__init__.py
+++ heudiconv/__init__.py
@@ -10,3 +10,20 @@ logging.basicConfig(
     level=getattr(logging, os.environ.get('HEUDICONV_LOG_LEVEL', 'INFO'))
 )
 lgr.debug("Starting the abomination")  # just to "run-test" logging
+
+try:
+    tmpdirs
+except NameError:
+    import atexit
+    tmpdirs = set()
+
+    def _clean_tmpdirs():
+        """Cleanup tracked temporary directories"""
+    import shutil
+
+    for tmpdir in tmpdirs:
+        try:
+            shutil.rmtree(tmpdir)
+        except FileNotFoundError:
+            pass
+    atexit.register(_clean_tmpdirs)
