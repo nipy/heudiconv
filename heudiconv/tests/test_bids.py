2bccb7c5c2f0bcb21ce5f80ad80dcb9816f5ce5b
pvelasco <pablo.velasco@nyu.edu>
2022-02-28 15:42:30 -0500

    Add test for the dataset that raised #541

--- heudiconv/tests/test_bids.py
+++ heudiconv/tests/test_bids.py
@@ -5,6 +5,13 @@ from heudiconv.bids import (
     maybe_na,
     treat_age,
 )
+from heudiconv.cli.run import main as runner
+from .utils import TESTS_DATA_PATH
+
+from os.path import (
+    join as pjoin,
+    exists as pexists,
+)
 
 
 def test_maybe_na():
@@ -24,3 +31,27 @@ def test_treat_age():
     assert treat_age('12M') == '1'
     assert treat_age('0000.1') == '0.1'
     assert treat_age(0000.1) == '0.1'
+
+
+def test_ME_mag_phase_conversion(tmpdir):
+    """ Unit test for the case of multi-echo GRE data with
+    magnitude and phase.
+    The different echoes should be labeled automatically.
+    """
+    tmppath = tmpdir.strpath
+    subID = 'MEGRE'
+    args = (
+        "-c dcm2niix -o %s -b -f bids_ME --files %s -s %s"
+        % (tmpdir, pjoin(TESTS_DATA_PATH, subID), subID)
+    ).split(' ')
+    runner(args)
+
+    # Check that the expected files have been extracted.
+    # This also checks that the "echo" entity comes before "part":
+    for part in ['mag', 'phase']:
+        for e in range(1,4):
+            for ext in ['nii.gz', 'json']:
+                assert pexists(
+                    pjoin(tmppath, 'sub-%s', 'anat', 'sub-%s_echo-%s_part-%s_MEGRE.%s')
+                    % (subID, subID, e, part, ext)
+                )
