401069bc459bccb13e99d6cacbc57776480dec43
Michael Dayan <michael.dayan@fcbg.ch>
2022-10-05 17:10:39 +0200

    Change neurodocker factory to set version of miniconda in order to set version of conda

--- utils/gen-docker-image.sh
+++ utils/gen-docker-image.sh
@@ -11,7 +11,7 @@ docker run --rm $image generate docker -b neurodebian:bullseye -p apt \
     --dcm2niix version=v1.0.20211006 method=source \
     --install git gcc pigz liblzma-dev libc-dev git-annex-standalone netbase \
     --copy . /src/heudiconv \
-    --miniconda version=latest conda_install="python=3.7 traits>=4.6.0 scipy numpy nomkl pandas" \
+    --miniconda version="py39_4.12.0" conda_install="python=3.7 traits>=4.6.0 scipy numpy nomkl pandas" \
       pip_install="/src/heudiconv[all]" \
       pip_opts="--editable" \
     --entrypoint "heudiconv" \
