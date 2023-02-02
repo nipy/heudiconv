fceb910fa25cc97a391d4231f20f76775e7161c5
Horea Christian <chr@chymera.eu>
2023-02-01 21:53:36 -0500

    committer name and email

--- heudiconv/tests/conftest.py
+++ heudiconv/tests/conftest.py
@@ -1,3 +1,8 @@
 import os
-os.environ["GIT_AUTHOR_EMAIL"] = "maxm@example.com"
-os.environ["GIT_AUTHOR_NAME"] = "Max Mustermann"
+import pytest
+@pytest.fixture(autouse=True, scope="session")
+def git_env():
+       os.environ["GIT_AUTHOR_EMAIL"] = "maxm@example.com"
+       os.environ["GIT_AUTHOR_NAME"] = "Max Mustermann"
+       os.environ["GIT_COMMITTER_EMAIL"] = "maxm@example.com"
+       os.environ["GIT_COMMITTER_NAME"] = "Max Mustermann"
