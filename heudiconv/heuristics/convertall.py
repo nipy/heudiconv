fc820051bb39e22a66b2f4816de0f96a086e7532
basile <basile.pinsard@gmail.com>
2023-02-16 10:45:16 -0500

    test data include messed-up dicoms with no affine

--- heudiconv/heuristics/convertall.py
+++ heudiconv/heuristics/convertall.py
@@ -21,7 +21,12 @@ def custom_seqinfo(wrapper: dw.Wrapper, series_files: list[str], **kw: Any) -> t
     # for already loaded DICOM data, and including storing/returning
     # the sample series file as was requested
     # in https://github.com/nipy/heudiconv/pull/333
-    return wrapper.affine.tostring(), series_files[0]
+    from nibabel.nicom.dicomwrappers import WrapperError
+    try:
+        affine = wrapper.affine.tostring()
+    except WrapperError:
+        affine = None
+    return affine, series_files[0]
 
 
 def infotodict(
