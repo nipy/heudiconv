ec4095a8fafb5ccc4e3ba0a7cb332c2c0112e6ab
Horea Christian <chr@chymera.eu>
2023-03-03 12:52:13 -0500

    Updated figure style and version tracking figure

--- scripts/downloads.py
+++ scripts/downloads.py
@@ -1,28 +1,35 @@
 import pandas as pd
 import seaborn as sns
-from matplotlib import pyplot
-sns.set_theme(style="ticks", palette="pastel")
+import matplotlib.pyplot as plt
+sns.set_theme(style='whitegrid')
+#sns.set_theme(style='ticks', palette='muted')
 
-df = pd.read_csv("../data/pypi-bigquery-results.csv",
+df = pd.read_csv('../data/pypi-bigquery-results.csv',
 	index_col=1,
 	parse_dates=True,
 	)
-df = df.rename(columns={"num_downloads":"downloads"})
-df['year'] = df.index.year
-df['month'] = df.index.month
+df = df.rename(columns={'num_downloads':'Weekly Downloads'})
+df['Year'] = df.index.year
+df['Month'] = df.index.month
 
 # Only whole years, hard-coding for now.
-df = df[~df['year'].isin([2017,2023])]
+df = df[~df['Year'].isin([2017,2023])]
 
 print(df.columns)
 print(df)
 
 
-sns.boxplot(data=df,
-	x="month",
-	y="downloads",
-	hue="year",
+#sns.boxplot(data=df,
+#	x='Month',
+#	y='Yearly Downloads',
+#	hue='Year',
+#	)
+sns.swarmplot(data=df,
+	x='Month',
+	y='Weekly Downloads',
+	hue='Year',
+	palette='flare',
 	)
-#sns.despine(offset=10, trim=True)
+plt.yscale('log')
 
-pyplot.savefig("../figs/downloads.pdf")
+plt.savefig('../figs/downloads.pdf')
