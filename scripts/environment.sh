a264f2829142465a4bb090c8d1066210496f976a
Horea Christian <chr@chymera.eu>
2024-01-04 21:00:44 -0500

    New figure style and PNG outputs as per yarik

--- scripts/environment.sh
+++ scripts/environment.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+dot -Tpng ../data/environment.dot -o ../figs/environment.png
 dot -Tpdf ../data/environment.dot -o ../figs/environment.pdf
--- scripts/workflow.sh
+++ scripts/workflow.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 dot -Tpdf ../data/workflow.dot -o ../figs/workflow.pdf
+#dot -Tpng ../data/workflow.dot -o ../figs/workflow.png
