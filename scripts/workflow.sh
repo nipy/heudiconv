4c601b2eb58f05edd8c4e4dc6d24c5d249cccd4c
Horea Christian <chr@chymera.eu>
2024-01-04 21:02:27 -0500

    Uncommented PNG generation

--- scripts/workflow.sh
+++ scripts/workflow.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 dot -Tpdf ../data/workflow.dot -o ../figs/workflow.pdf
-#dot -Tpng ../data/workflow.dot -o ../figs/workflow.png
+dot -Tpng ../data/workflow.dot -o ../figs/workflow.png
