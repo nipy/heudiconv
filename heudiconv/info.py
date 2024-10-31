d121ae10b07405dcf0e4a0ed522ec6f29893c1ff
Yaroslav Halchenko <debian@onerussian.com>
2024-10-31 14:20:05 -0400

    chore!: drop support and testing for EOLed Python 3.8

--- heudiconv/info.py
+++ heudiconv/info.py
@@ -11,15 +11,15 @@ CLASSIFIERS = [
     "Environment :: Console",
     "Intended Audience :: Science/Research",
     "License :: OSI Approved :: Apache Software License",
-    "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3.10",
     "Programming Language :: Python :: 3.11",
+    "Programming Language :: Python :: 3.12",
     "Topic :: Scientific/Engineering",
     "Typing :: Typed",
 ]
 
-PYTHON_REQUIRES = ">=3.8"
+PYTHON_REQUIRES = ">=3.9"
 
 REQUIRES = [
     # not usable in some use cases since might be just a downloader, not binary
